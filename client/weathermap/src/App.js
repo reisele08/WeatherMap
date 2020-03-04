0302211e2384eda70411978897fc020e8978280a
Richard Eisele <reisele@sfu.ca>
2020-03-04 12:50:07 -0800

    temp remove leaflet and added browser routing/links

--- client/weathermap/src/App.js
+++ client/weathermap/src/App.js
@@ -2,28 +2,54 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserList from './components/UserList'
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { apiResponse: "" };
+  // }
 
-  callAPI() {
-      fetch("http://localhost:9000/testAPI")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
-  }
+  // callAPI() {
+  //     fetch("http://localhost:9000/testAPI")
+  //         .then(res => res.text())
+  //         .then(res => this.setState({ apiResponse: res }));
+  // }
 
-  componentWillMount() {
-      this.callAPI();
-  }
+  // componentWillMount() {
+  //     this.callAPI();
+  // }
+
+
 
   render() {
     return (
-<div>
-  <UserList />
-</div>
+      <div>
+          <Layout>
+              <Header className = "header-color" title = "Title">
+                  <Navigation>
+                      <Link to = "/">Home</Link>
+                      <Link to="/login">Login</Link>
+                      <Link to="/UserList">UserList</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Header>
+              <Drawer className = "drawer-color" title= "Drawer Title">
+                  <Navigation>
+                      <Link to="/resume">Link</Link>
+                      <Link to="/aboutme">Link</Link>
+                      <Link to="/projects">Link</Link>
+                      <Link to="/contact">Link</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Main/>
+              </Content>     
+          </Layout>
+      </div>
     );
   }
 }
