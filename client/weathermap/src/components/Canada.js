fae01dab1cf7baf59797cfefca2186410861f2a1
jimmyz <jimmyz@sfu.ca>
2020-04-01 22:34:10 -0700

    working graph but x axis not correct format

--- client/weathermap/src/components/Canada.js
+++ client/weathermap/src/components/Canada.js
@@ -4,6 +4,7 @@ import Chart from 'react-apexcharts';
 
 import requestServer from "./RequestServer";
 
+//
 class ApexChart extends Component {
     constructor(props) {
         super(props);
@@ -11,12 +12,9 @@ class ApexChart extends Component {
 
 
         this.state = {
-
-                received:false,
-
                 series: [{
-                    name: "Desktops",
-                    data: [ ]//************Here ***************** is where you set the Y data
+                    name: "active cases",
+                    data:[]//************Here ***************** is where you set the Y data
                 }],
                 options: {
                     chart: {
@@ -27,14 +25,14 @@ class ApexChart extends Component {
                         }
                     },
                     dataLabels: {
-                        enabled: false
+                        enabled: true
                     },
                     stroke: {
                         curve: 'straight'
                     },
                     title: {
                         text: 'Canada confirmed cases',
-                        align: 'left'
+                        align: 'center'
                     },
                     grid: {
                         row: {
@@ -53,34 +51,45 @@ class ApexChart extends Component {
     componentDidMount() {
         this.getData();
         //need to retrun it as 2arrays;
-
     }
-
     async getData() {
         var response = await requestServer.getCanadaConfirmed();
         console.log(response.data);
         this.updateGraphData(response.data)
-
     }
 
+    formatDate(dateTimeArray) {
+        var formated = [];
+
+        dateTimeArray.map((date) => {
+            var splitdate = date.split("T");
+            //var formatDate = new Date(splitdate[0].replace(/-/g,"/")).getTime();
+            var formated = splitdate[0].replace(/-/g,"/");
+            return formated;
+
+        });
+        console.log(dateTimeArray)
+        return dateTimeArray;
+
+    }
     updateGraphData(controllerCall) {
 
         var options = {...this.state.options};
-        options.xaxis.categories = controllerCall.dates;
-
-        this.setState({options});
+        options.xaxis.categories = this.formatDate(controllerCall.dates);
+        this.setState( options);
 
         var series = {...this.state.series};
-        series.data = controllerCall.values;
-        this.setState({series});
+        series[0].data = controllerCall.values;
+        series[0].name = "active Cases";
 
-        this.setState({reveived:true});
+        this.setState(series);
+
 
     }
 
     render() {
         let graph;//conditions?
-        graph = <Chart options={this.state.options} series={this.state.series} type="line" height={350} />
+        graph = <Chart options = {this.state.options} series = {this.state.series} type="line" height={350} />
         return (
 
             <div id="chart">
