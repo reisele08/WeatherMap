ef3b16764db756b343cd8618bf2587fc17767e9d
Gaeun Lee <gaeunl@sfu.ca>
2020-04-02 11:36:08 -0700

    use png file from images folder

--- client/weathermap/src/components/icon.js
+++ client/weathermap/src/components/icon.js
@@ -37,8 +37,8 @@ var yellowIcon = new L.Icon({
 });
 
 var redIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconUrl: require('./images/marker-icon-2x-red.png'),
+    shadowUrl: null,
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
@@ -54,50 +54,5 @@ var greyIcon = new L.Icon({
     shadowSize: [41, 41]
 });
 
-//    iconUrl: './leaf-green.png',
-// var goldIcon = new L.Icon({
-//     iconUrl: '/img/marker-icon-2x-gold.png',
-//     shadowUrl: 'img/marker-shadow.png',
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     shadowSize: [41, 41]
-// });
-//
-// var orangeIcon = new L.Icon({
-//     iconUrl: 'img/marker-icon-2x-orange.png',
-//     shadowUrl: 'img/marker-shadow.png',
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     shadowSize: [41, 41]
-// });
-//
-// var violetIcon = new L.Icon({
-//     iconUrl: 'img/marker-icon-2x-violet.png',
-//     shadowUrl: 'img/marker-shadow.png',
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     shadowSize: [41, 41]
-// });
-//
-// var greyIcon = new L.Icon({
-//     iconUrl: 'img/marker-icon-2x-grey.png',
-//     shadowUrl: 'img/marker-shadow.png',
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     shadowSize: [41, 41]
-// });
-//
-// var blackIcon = new L.Icon({
-//     iconUrl: 'img/marker-icon-2x-black.png',
-//     shadowUrl: 'img/marker-shadow.png',
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     shadowSize: [41, 41]
-// });
 
 export { blackIcon,blueIcon,greenIcon,greyIcon,redIcon,yellowIcon};
